f6c92e9e0b01680d0c715fc93c970992781f8904
Alex Beshine <alex31n@users.noreply.github.com>
2018-05-03 18:30:43 +0600

    Add files via upload

---
+++ richtext/src/main/java/com/ornach/richtext/RichText.java
@@ -0,0 +1,142 @@
+package com.ornach.richtext;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Build;
+import android.support.annotation.ColorInt;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+
+public class RichText extends TextView {
+
+	int borderWidth = 0;
+	@ColorInt
+	int borderColor = 0x000000;
+	float radius = 0;
+
+	@ColorInt
+	int backgroundColor = 0x000000;
+	//int backgroundColor = Color.GREEN;
+
+
+	private int textColor = 0;
+
+	public RichText(Context context) {
+		super(context);
+
+		initializeView();
+	}
+
+	public RichText(Context context, @Nullable AttributeSet attrs) {
+		super(context, attrs);
+
+		processAttributes(context, attrs);
+		initializeView();
+	}
+
+	public RichText(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+
+		processAttributes(context, attrs);
+		initializeView();
+	}
+
+	@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+	public RichText(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+		super(context, attrs, defStyleAttr, defStyleRes);
+
+		processAttributes(context, attrs);
+		initializeView();
+	}
+
+
+	private void processAttributes(final Context context, final AttributeSet attrs) {
+		TypedArray attrsArray = context.obtainStyledAttributes(attrs, R.styleable.RichText, 0, 0);
+		initAttributes(attrsArray);
+		attrsArray.recycle();
+	}
+
+	private void initAttributes(TypedArray attrs) {
+
+		radius = attrs.getDimension(R.styleable.RichEditText_rt_radius, radius);
+		borderColor = attrs.getColor(R.styleable.RichEditText_rt_borderColor, borderColor);
+		borderWidth = (int) attrs.getDimension(R.styleable.RichEditText_rt_borderWidth, borderWidth);
+		backgroundColor = attrs.getColor(R.styleable.RichEditText_rt_backgroundColor, backgroundColor);
+		//Log.e("TAG", "backgroundColor "+ backgroundColor);
+
+	}
+
+
+	private void initializeView() {
+		//storeDefaultValue();
+
+
+		setupBackground();
+	}
+
+	private void storeDefaultValue() {
+		textColor = getCurrentTextColor();
+	}
+
+
+	private void setupBackground() {
+
+		DrawableHelper helper = new DrawableHelper.Builder()
+			  .setBackgroundColor(backgroundColor)
+			  .setDisabledColor(0)
+			  .setBorderWidth(borderWidth)
+			  .setBorderColor(borderColor)
+			  .setDisabledBorderColor(0)
+			  .setRadius((int) radius)
+			  .setEnabled(true)
+			  .build();
+		helper.setBackground(this);
+
+
+	}
+
+
+	public void setBorderWidth(int borderWidth){
+		this.borderWidth = borderWidth;
+		setupBackground();
+	}
+	public int getBorderWidth() {
+		return borderWidth;
+	}
+
+
+	public void setBorderColor(int color) {
+		this.borderColor = color;
+		setupBackground();
+	}
+
+	public int getBorderColor() {
+		return this.borderColor;
+	}
+
+
+	public void setRadius(float size) {
+		this.radius = size;
+		setupBackground();
+	}
+
+	public float getRadius() {
+		return radius;
+	}
+
+	public void setBackgroundColor(int color) {
+		this.backgroundColor = color;
+		setupBackground();
+	}
+
+	public int getBackgroundColor() {
+		return this.backgroundColor;
+	}
+
+
+
+
+}
---
+++ richtext/src/main/java/com/ornach/richtext/RichView.java
@@ -0,0 +1,85 @@
+package com.ornach.richtext;
+
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Build;
+import android.support.annotation.ColorInt;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class RichView extends LinearLayout{
+
+	int borderWidth = 0;
+	@ColorInt
+	int borderColor = 0x000000;
+	float radius = 0;
+
+	@ColorInt
+	int backgroundColor = 0x000000;
+
+	public RichView(Context context) {
+		super(context);
+
+		initializeView();
+	}
+
+	public RichView(Context context, @Nullable AttributeSet attrs) {
+		super(context, attrs);
+
+		processAttributes(context, attrs);
+		initializeView();
+	}
+
+	public RichView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+		processAttributes(context, attrs);
+		initializeView();
+	}
+
+	@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+	public RichView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+		super(context, attrs, defStyleAttr, defStyleRes);
+		processAttributes(context, attrs);
+		initializeView();
+	}
+
+
+	private void processAttributes(final Context context, final AttributeSet attrs) {
+		TypedArray attrsArray = context.obtainStyledAttributes(attrs, R.styleable.RichText, 0, 0);
+		initAttributes(attrsArray);
+		attrsArray.recycle();
+	}
+
+	private void initAttributes(TypedArray attrs) {
+
+		radius = attrs.getDimension(R.styleable.RichEditText_rt_radius, radius);
+		borderColor = attrs.getColor(R.styleable.RichEditText_rt_borderColor, borderColor);
+		borderWidth = (int) attrs.getDimension(R.styleable.RichEditText_rt_borderWidth, borderWidth);
+		backgroundColor = attrs.getColor(R.styleable.RichEditText_rt_backgroundColor, backgroundColor);
+		//Log.e("TAG", "backgroundColor "+ backgroundColor);
+
+	}
+
+	private void initializeView() {
+		setupBackground();
+	}
+
+	private void setupBackground() {
+
+		DrawableHelper helper = new DrawableHelper.Builder()
+			  .setBackgroundColor(backgroundColor)
+			  .setDisabledColor(0)
+			  .setBorderWidth(borderWidth)
+			  .setBorderColor(borderColor)
+			  .setDisabledBorderColor(0)
+			  .setRadius((int) radius)
+			  .setEnabled(true)
+			  .build();
+		helper.setBackground(this);
+
+
+	}
+}
